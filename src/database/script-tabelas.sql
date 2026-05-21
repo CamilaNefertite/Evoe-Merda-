657e5b39ed6d4d0555acd64dc33959fc583ca426
CamilaNefertite <camila.cardoso@sptech.school>
2026-05-21 20:20:43 -0300

    atualizando os ajustes que eu fiz

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,46 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE DATABASE dbDesatino;
+USE dbDesatino;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+  idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  dtNasc DATE,
+  perfil VARCHAR(15) NOT NULL,
+	CONSTRAINT chkPerfil
+		CHECK (perfil IN ('ator', 'espectador', 'curioso')),
+  email VARCHAR(50) NOT NULL UNIQUE,
+  senha VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE resultadoQuiz (
+  idresultadoQuiz INT PRIMARY KEY AUTO_INCREMENT,
+  resultado VARCHAR(45) NULL,
+  fkUsuario INT NOT NULL,  
+  CONSTRAINT fkResultadoUsuario
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE artigo (
+  idArtigo INT PRIMARY KEY AUTO_INCREMENT,
+  titulo VARCHAR(45) NOT NULL,
+  conteudo VARCHAR(3000) NOT NULL,
+  imagem VARCHAR(60) NULL,
+  dtPublicacao DATE NOT NULL DEFAULT(CURRENT_DATE),
+  categoria VARCHAR(45) NOT NULL
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+CREATE TABLE favoritos (
+  idFavoritos INT PRIMARY KEY AUTO_INCREMENT,
+  idUsuario INT NOT NULL,
+  idArtigo INT NOT NULL,
+  CONSTRAINT fkIdUsuarios
+    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
+  CONSTRAINT fkIdArtigo
+    FOREIGN KEY (idArtigo) REFERENCES artigo(idArtigo)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+-- CRIANDO O ADM
+INSERT INTO usuario (nome, dtNasc, perfil, email, senha) VALUES 
+	('Camila Nefertite', '2008-01-25', 'ator', 'camila.cardoso@sptech.school', 'senha123');
+    
